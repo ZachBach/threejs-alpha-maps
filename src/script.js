3b6f062ca81b9eaab0ad2f65c96160b600dc7066
Zach Auerbach <auernachzach@gmail.com>
2021-09-18 19:40:43 -0700

    added alpha and height pngs from photoshop

--- src/script.js
+++ src/script.js
@@ -3,6 +3,12 @@ import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Texture Loader
+const loader = new THREE.TextureLoader()
+const height = loader.load('height.png')
+const texture = loader.load('/texture.jpg')
+const alpha = loader.load('alpha.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -19,6 +25,12 @@ const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
 // Materials
 const material = new THREE.MeshStandardMaterial({
     color: "gray",
+    map: texture,
+    displacementMap: height,
+    displacementScale: .7,
+    alphaMap: alpha,
+    transparent: true,
+    depthTest: false
 })
 
 const plane = new THREE.Mesh(geometry,material);
@@ -36,10 +48,10 @@ gui.add(plane.rotation, 'x').min(0).max(600);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 2)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight('#00ff00', 2)
+pointLight.position.x = .2
+pointLight.position.y = 10
+pointLight.position.z = 4.4
 scene.add(pointLight)
 
 gui.add(pointLight.position, "x")
@@ -94,7 +106,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -102,6 +115,14 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+
+document.addEventListener('mousemove', animateTerrain)
+
+let mouseY = 0
+
+function animateTerrain(event) {
+    mouseY = event.clientY
+}
 
 const clock = new THREE.Clock()
 
@@ -113,6 +134,8 @@ const tick = () =>
     // Update objects
     // sphere.rotation.y = .5 * elapsedTime
 
+    plane.rotation.z = .5 * elapsedTime
+    plane.material.displacementScale = .5 + mouseY * 0.0008
     // Update Orbital Controls
     // controls.update()
 
